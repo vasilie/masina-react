f324281ce3b53fd8c653162b19e96596cede8dcf
_vasilie <vasilije.milenkovic@diwanee.com>
2018-01-16 16:13:46 +0100

    second inits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import Projects from "./Components/Projects";
+import AddProject from "./Components/AddProject";
 import './App.css';
 
 class App extends Component {
+  constructor(){
+      super();
+      this.state = {
+          projects: []
+      }
+  }
+  componentWillMount(){
+      this.setState({projects:[
+          {
+              title: "Business",
+              category: "Web Design"
+          },
+          {
+              title: "Social App",
+              category: "Mobile Design"
+          },
+          {
+              title: "Ecommerce Shopping cart",
+              category: "Web Developent"
+          }
+      ]});
+  }
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
   render() {
     return (
       <div className="App">
-           My App
+           <AddProject addProject = {this.handleAddProject.bind(this)}></AddProject>
+           <Projects projects={this.state.projects}/>
       </div>
     );
   }
