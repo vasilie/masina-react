14b90d35d391ca7e3f974f93ed94802da696a0ac
_vasilie <vasilije.milenkovic@diwanee.com>
2018-02-01 14:55:38 +0100

    login Form errors, data passed to console after submit

--- src/Components/pages/LoginPage.js
+++ src/Components/pages/LoginPage.js
@@ -2,11 +2,20 @@ import React from "react";
 import LoginForm from "../forms/LoginForm";
 // import 'semantic-ui-css/semantic.min.css';
 
-const LoginPage = () => (
-  <div>
-    login page
-    <LoginForm />
-  </div>
-);
+class LoginPage extends React.Component {
+  submit = (data) =>{
+    console.log(data);
+  }
+  render(){
+
+    return (
+      <div className='wrapper'>
+        Login page
+        <br />
+        <LoginForm submit={this.submit} />
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
