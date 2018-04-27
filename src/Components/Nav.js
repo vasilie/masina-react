4a52e2190a134f2914dcaecf53754d5752c4bfba
_vasilie <vasilije.milenkovic@diwanee.com>
2018-04-27 16:59:51 +0200

    links removed

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,10 +6,7 @@ class Nav extends Component {
     return (
       <ul className="header__nav">
         <li className="header__nav__item"><NavLink to="/" activeClassName='active'>Home</NavLink></li>
-        <li className="header__nav__item"><NavLink to="/about" activeClassName='active'>About</NavLink></li>
         <li className="header__nav__item"><NavLink to="/chatPage" activeClassName='active'>Chat</NavLink></li>
-        <li className="header__nav__item"><NavLink to="/contact" activeClassName='active'>Contact</NavLink></li>
-        <li className="header__nav__item"><NavLink to="/artpage" activeClassName='active'>Art</NavLink></li>
       </ul>
     );
   }
